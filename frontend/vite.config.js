a9a06fc67097bb1eb66179087525bf12bbc780fc
JuanFig023 <juanseb023@gmail.com>
2025-12-11 13:12:17 -0500

    Add preview configuration to Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: true,
+  },
 })
